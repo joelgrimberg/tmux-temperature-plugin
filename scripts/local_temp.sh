db3bbb57f58505b6291cab12ab72303bf5819f26
J. Grimberg <github@joelgrimberg.nl>
2024-08-20 07:59:35 +0200

    trying out

--- scripts/local_temp.sh
+++ scripts/local_temp.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-tmux bind-key T run-shell "$CURRENT_DIR/scripts/tmux_list_plugins.sh"
+#CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#tmux bind-key T run-shell "$CURRENT_DIR/scripts/local_temp.sh"
 
 #curling the temp API
 
-#curl -s http://192.168.1.229:3005 > /dev/null | jq .tmp
+TEMP="$( curl -s http://192.168.1.229:3005 > /dev/null | jq .tmp)"
+tmux bind-key T run-shell "$CURRENT_DIR/scripts/local_temp.sh"
