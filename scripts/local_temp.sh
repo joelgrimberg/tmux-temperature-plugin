530acb5629c52b1766c1263da853c0aa34ddeba2
J. Grimberg <github@joelgrimberg.nl>
2024-08-20 11:40:05 +0200

    oopsie

--- scripts/local_temp.sh
+++ scripts/local_temp.sh
@@ -5,5 +5,5 @@
 
 #curling the temp API
 
-TEMP="$( curl -s http://192.168.1.229:3005 > /dev/null | jq .tmp)"
+TEMP="$( curl -s http://192.168.1.229:3005 | jq .tmp)"
 tmux bind-key T run-shell "$CURRENT_DIR/scripts/local_temp.sh"
