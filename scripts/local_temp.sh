5b46537c7d4425f9df41662b8f3ce58a83bd9179
J. Grimberg <github@joelgrimberg.nl>
2024-08-20 12:21:33 +0200

    more iterations

--- scripts/local_temp.sh
+++ scripts/local_temp.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-#CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#tmux bind-key T run-shell "$CURRENT_DIR/scripts/local_temp.sh"
-
 #curling the temp API
 
 TEMP="$( curl -s http://192.168.1.229:3005 | jq .tmp)"
